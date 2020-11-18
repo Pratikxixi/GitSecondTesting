7801919ddea8ed8d49569d445eb698eb96e5c3cd
Pratik Chauhan <pratik.chauhan@raweng.com>
2020-11-18 15:24:50 +0530

    m3

--- app/src/main/java/com/raweng/gitsecondtesting/MainActivity.kt
+++ app/src/main/java/com/raweng/gitsecondtesting/MainActivity.kt
@@ -9,5 +9,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val m1 = "m1"
         val m2  ="m2"
+        val m3 = "m3"
+        val f1 = "f1"
+        val f2 = "f2"
+        val f4= "f4"
+        val f5= "f5"
+        val f6= "f6"
+
     }
 }
